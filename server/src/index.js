712e43abe498a737149d12058800104ecb19b6d6
Fabian <fabianvillagra28@gmail.com>
2023-10-09 02:28:37 -0300

    fix: se agregó en el index las importaciones faltantes

--- server/src/index.js
+++ server/src/index.js
@@ -1,12 +1,27 @@
 import express from 'express';
 import morgan from 'morgan';
+import fileUpload from 'express-fileupload';
+import path from "path";
 import { environment } from './config/environments.js';
 import { connectDB } from './config/connection.js';
+
+import fileDirName from "./utils/fileDirName.js";
+const { __dirname } = fileDirName(import.meta);
+
+import AuthorRouter from './routes/author.routes.js';
+import GenreRouter from './routes/genre.routes.js';
+import BookRouter from './routes/book.routes.js';
 
 const app = express();
 
 app.use(morgan('dev'));
 app.use(express.json());
+app.use(fileUpload());
+app.use(express.static(path.join(__dirname, "../files")));
+
+app.use("/authors", AuthorRouter);
+app.use("/genres", GenreRouter);
+app.use("/books", BookRouter);
 
 
 const port = environment.PORT || 4000;
